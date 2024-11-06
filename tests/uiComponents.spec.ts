63320ab0e4550c1db4c943ce7a8e18e442919d4d
Michael Naghavipour <m.naghavipour@reply.de>
2024-11-06 13:49:04 -0500

    lists and dropdowns

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -58,3 +58,30 @@ test('checkboxes', async ({ page }) => {
         expect(await box.isChecked()).toBeFalsy
     }
 })
+
+test('Lists and Dropdowns', async ({ page }) => {
+    const dropDownMenu = page.locator('ngx-header nb-select')
+    await dropDownMenu.click()
+
+    // await page.locator('ngx-header nb-select').click()
+    const optionList = page.locator('nb-option-list nb-option')
+    await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"])
+    await optionList.filter({hasText: "Dark"}).click()
+    const header = page.locator('nb-layout-header')
+    await expect(header).toHaveCSS('background-color', 'rgb(34, 43, 69)')
+
+    const colors = {
+        "Light": "rgb(255, 255, 255)",
+        "Dark": "rgb(34, 43, 69)",
+        "Cosmic": "rgb(50, 50, 89)",
+        "Corporate": "rgb(255, 255, 255)"
+    }
+
+    await dropDownMenu.click()
+    for(const color in colors) {
+        await optionList.filter({hasText: color}).click()
+        await expect(header).toHaveCSS('background-color', colors[color])
+        if(color !== 'Corporate')
+            await dropDownMenu.click()
+    }
+})
