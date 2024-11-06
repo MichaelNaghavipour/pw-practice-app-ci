88d63fa1297c2dfd4611f19c4d344a9fb7b960e7
Michael Naghavipour <m.naghavipour@reply.de>
2024-11-06 12:36:17 -0500

    add radio buttons and checkbox tests

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -25,4 +25,36 @@ test.describe('Form Layouts Page', () => {
         // locator assertion
         await expect(usingTheGridEmailInput).toHaveValue('test@gmail.com')
     })
+
+    test('Radio buttons', async ({ page }) => {
+        const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+        // await usingTheGridForm.getByLabel('Option 1').check({ force: true })
+        await usingTheGridForm.getByRole('radio', { name: "Option 1"}).check({ force: true })
+        const radioStatus = await usingTheGridForm.getByRole('radio', { name: "Option 1"}).isChecked()
+        expect(radioStatus).toBeTruthy()
+        await expect(usingTheGridForm.getByRole('radio', { name: "Option 1"})).toBeChecked()
+
+        await usingTheGridForm.getByRole('radio', { name: "Option 2"}).check({ force: true })
+        expect(await usingTheGridForm.getByRole('radio', { name: "Option 2"}).isChecked()).toBeTruthy()
+        expect(await usingTheGridForm.getByRole('radio', { name: "Option 1"}).isChecked()).toBeFalsy()
+    })
+})
+
+test('checkboxes', async ({ page }) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    await page.getByRole('checkbox', { name: "Hide on click"}).uncheck({ force: true })
+    await page.getByRole('checkbox', { name: "Prevent arising of duplicate toast"}).check({ force: true })
+
+    const allBoxes = page.getByRole('checkbox')
+    for (const box of await allBoxes.all()) {
+        await box.check({ force: true })
+        expect(await box.isChecked()).toBeTruthy()
+    }
+
+    for (const box of await allBoxes.all()) {
+        await box.uncheck({ force: true })
+        expect(await box.isChecked()).toBeFalsy
+    }
 })
